3e6e921ebdd9dec0f33ed1744840d1c098e78fe6
Veronika <veronikamart1994@gmail.com>
2022-05-31 23:20:01 +0300

    FIX Factory

--- Assets/DesignPatternsUnity/Factory/Scripts/Factory/GenericFactory.cs
+++ Assets/DesignPatternsUnity/Factory/Scripts/Factory/GenericFactory.cs
@@ -4,11 +4,11 @@ namespace DesignPatterns.Factory
 {
     public class GenericFactory<T> : MonoBehaviour where T : MonoBehaviour
     {
-        [SerializeField] private T obj;
+        [SerializeField] private T spawnedObject;
 
         public T InstantiateObject()
         {
-            return Instantiate(obj, transform);
+            return Instantiate(spawnedObject, transform);
         }
     }
 }
--- Assets/DesignPatternsUnity/Factory/Scripts/Objects/AbstractObject.cs
+++ Assets/DesignPatternsUnity/Factory/Scripts/Objects/AbstractObject.cs
@@ -2,9 +2,5 @@ using UnityEngine;
 
 namespace DesignPatterns.Factory
 {
-    public abstract class AbstractObject : MonoBehaviour
-    {
-        public new string name;
-        public int iD;
-    }
+    public abstract class AbstractObject : MonoBehaviour { }
 }
