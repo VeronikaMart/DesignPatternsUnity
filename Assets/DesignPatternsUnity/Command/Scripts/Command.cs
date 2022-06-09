505b5ceb50f0d57d6daca414b7f4cc8ea6a14cf8
Veronika <veronikamart1994@gmail.com>
2022-06-09 18:30:59 +0300

    ADD

--- Assets/DesignPatternsUnity/Command/Scripts/Command.cs
+++ Assets/DesignPatternsUnity/Command/Scripts/Command.cs
@@ -5,7 +5,7 @@ namespace DesignPatterns.Command
     // Provides an interface to perform an operation
     public abstract class Command
     {
-        protected float moveDistance = 3f; //How far should the object move
+        protected float moveDistance = 3f; // How far should the object move
 
         public abstract void Execute(Transform transform, Command command);
 
